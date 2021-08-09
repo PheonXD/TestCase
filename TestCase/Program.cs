2bc9c7dc9e747788121a914140c6049943f0bd9f
PheonXD <minnullin2@gmail.com>
2021-08-09 22:47:43 +0500

    update

--- TestCase/Program.cs
+++ TestCase/Program.cs
@@ -1,85 +1,47 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Text;
+
 namespace TestCase
 {
     class Program
     {
-        internal static string _minValue = "0000000000000";
-        internal static string _maxValue = "CCCCCCCCCCCCC";
+        internal static long summ = 0;
+
         static void Main(string[] args)
         {
-            BruteForce();
-        }
-
-        static ulong HexToDec(string value)
-        {
-            UInt64 res = 0;
-            UInt64 factor = 1;
-            for (int i = value.Length - 1; i >= 0; i--, factor *= 13)
-                if (value[i] >= '0' && value[i] <= '9') res += (UInt64)(value[i] - '0') * factor;
-                else
-                    if (value[i] >= 'A' && value[i] <= 'C') res += (UInt64)(value[i] - 55) * factor;
-            return res;
-        }
-
-        static string DecToHex(ulong value)
-        {
-            string res = "";
-            UInt64 factor = 1;
-            var number = Convert.ToInt64(value);
-            for (; number > 0; number /= 13)
+            while (true)
             {
-                factor = (ulong)(number % 13);
-                res = (char)(factor < 10 ? factor + '0' : factor + 55) + res;
+                Console.WriteLine("Введите кол-во цифр");
+                var countOfNumber = Console.ReadLine();
+                Start(Convert.ToInt32(countOfNumber));
             }
-            return res;
         }
 
-        static void BruteForce()
+        static void Start(int countOfNumber)
         {
-            var count = 0;
-            var dec = HexToDec(_maxValue) / 2;
-            var min = HexToDec(_minValue) / 2;
-            Console.WriteLine($"Started calculation beautifuls number from {_minValue} to hex {_maxValue} (Dec: {dec})");
-
-            for (var i = dec; i > min; i--)
+            var rank = (countOfNumber / 2) * 9 / 2;
+            for (int i = 0; i <= rank; i++)
             {
-                var hex = DecToHex(i);
-                var lastSix = hex.Remove(0, 7);
-                var firstSix = hex.Remove(6, 7);
-                if (CheckBeautifulNumber(lastSix, firstSix))
-                {
-                    count++;
-                    Console.WriteLine($"finded number: {hex}");
-                }
+                if (i == 0) summ = 1;
+                else if (i > 0)
+                    summ += getSumOfСombinations(i);
             }
-
-            Console.WriteLine($"Count of beautifuls number: {count}");
+            Console.WriteLine("кол-во красивых чисел: " + (summ * 2).ToString());
         }
 
-        static int[] GetIntArray(ulong num)
+        static long getSumOfСombinations(int rank)
         {
-            List<int> listOfInts = new List<int>();
-            while (num > 0)
-            {
-                listOfInts.Add((int)num % 10);
-                num = num / 10;
+            rank += 2;
+            var combinations = (rank * (rank - 1)) / (2);
+            if (rank >= 12)
+            {   
+                var rankDevider = (rank / 10 * 10) + 3;
+                var ten = (3 * ((rank - rankDevider) * (rank - rankDevider - 1)) / (2));
+                combinations = combinations - ten;
             }
-            listOfInts.Reverse();
-            return listOfInts.ToArray();
-        }
-
-        static bool CheckBeautifulNumber(string hexNumber1, string hexNumber2)
-        {
-            var firstArr = GetIntArray(HexToDec(hexNumber1));
-            var secondArr = GetIntArray(HexToDec(hexNumber2));
-
-            var summFirst = firstArr.Sum();
-            var summSecond = secondArr.Sum();
-
-            if (summFirst == summSecond) return true;
-            else return false;
+            return combinations * combinations;
         }
     }
 }
